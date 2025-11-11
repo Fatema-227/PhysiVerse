6ebd7f05dab9cebb17f504683e0e0343569e07a8
Fatema-227 <fat00m.alshughul.2oo5@gmail.com>
2025-11-11 12:38:19 +0300

    models lab and experiment is done

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Lab(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(max_length=100)
+    image=models.ImageField(upload_to='main_app/static/uploads/',default="")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Experiment(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField(max_length=100)
+    Simulation_Data=models.TextField()
+    image=models.ImageField(upload_to='main_app/static/image/',default="")
+    video=models.FileField(upload_to='main_app/static/videos/',default="")
+    created_at=models.DateTimeField(auto_now_add=True)
+    lab=models.ForeignKey(Lab,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
